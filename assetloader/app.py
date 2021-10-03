dc264d5055d4eac4cd58c1e7b06a13efc38c7734
Jose <joseyose_dev@outlook.com>
2021-10-03 19:47:01 -0400

    remove some comments and clean up code

--- assetloader/app.py
+++ assetloader/app.py
@@ -5,14 +5,11 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication
-# from.views import Window
 from .views import Window
 
 def main():
-    # Create the application
     app = QApplication(sys.argv)
-    # Create and show the main window
     win = Window()
     win.show()
-    # Run the event loop
+
     sys.exit(app.exec())
